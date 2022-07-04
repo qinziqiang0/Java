daf01e55c188eab58a7007d4fe567ad50c8e889d
qinziqiang0 <1930134590@qq.com>
2022-07-04 12:55:02 +0800

    e

---
+++ a.java
@@ -0,0 +1,45 @@
+/*
+一个Java项目
+*/
+
+//a类
+public class a {
+    //main方法
+    public static void main(String[] args) {
+        //输出Hello World
+        System.out.println("Hello World");
+        System.out.println("---------");
+        
+        //字符串常量
+        System.out.println("Hello World");
+        System.out.println("JAVA语言");
+        System.out.println("---------");
+        
+        //整数常量
+        System.out.println(666);
+        System.out.println(-15975346280);
+        System.out.println("---------");
+        
+        //小数常量
+        System.out.println(3.141592654);
+        System.out.println(8848.86);
+        System.out.println("---------");
+        
+        //字符常量
+        System.out.println('G');
+        System.out.println('7');
+        System.out.println('一');
+        System.out.println("---------");
+        
+        //布尔常量
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println("---------");
+        
+        //空常量
+        //空常量不能直接输出
+        //System.out.println(null);
+        System.out.println("---------");
+        
+    }
+}
