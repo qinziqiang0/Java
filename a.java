f1dea43cfe970a702cf7097fbf9b6ef79051ded1
qinziqiang0 <1930134590@qq.com>
2022-07-05 17:02:56 +0800

    e

--- a.java
+++ a.java
@@ -17,7 +17,7 @@ public class a {
         
         //整数常量
         System.out.println(666);
-        System.out.println(-15975346280);
+        System.out.println(-314);
         System.out.println("---------");
         
         //小数常量
@@ -41,5 +41,25 @@ public class a {
         //System.out.println(null);
         System.out.println("---------");
         
+        //定义变量 变量类型有{byte,short,int,long,float,double,char,boolean}
+        int a = 7;
+        //输出变量
+        System.out.println(a);
+        //修改变量
+        a = 12;
+        System.out.println(a);
+        System.out.println("---------");
+        long z = 214748364748L;
+        float y = 3.141592654F;
+        System.out.println(z);
+        System.out.println(y);
+        System.out.println("---------");
+        /*
+        标识符
+        由{数字,字母,下划线(_),美元符($)}组成
+        不能以数字开头
+        不能是关键字
+        区分大小写
+        */
     }
 }
